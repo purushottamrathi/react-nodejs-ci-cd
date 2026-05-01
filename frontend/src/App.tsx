3dc2cf52e125341f74bab0a741cbf161c3f9acaa
purushottamrathi <48144363+purushottamrathi@users.noreply.github.com>
2026-05-02 00:06:56 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -39,7 +39,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started Test Deploy</h1>
+          <h1>Get started Test Deploy 1</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
